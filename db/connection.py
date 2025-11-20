34794e2338b30deb35e1cbff95004ad57053d5d5
Lautaro Orellano <Lautaa123@gmail.com>
2025-11-19 23:35:01 -0300

    refactor: conection with sqlAlchemy

--- db/connection.py
+++ db/connection.py
@@ -1,10 +1,16 @@
-# db/connection.py
-import mysql.connector
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import settings
 
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/{settings.DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Dependency
 def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="clientes_db"
-    )
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
