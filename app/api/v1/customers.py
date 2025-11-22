83aba006bba220dd0adb8078a1f7bfc3b226749d
Lautaro Orellano <Lautaa123@gmail.com>
2025-11-21 23:31:02 -0300

    refactor: new logic for authenticaction

--- app/api/v1/customers.py
+++ app/api/v1/customers.py
@@ -4,37 +4,56 @@ from app.schemas.customer_schema import CustomerCreate, CustomerRead, CustomerQu
 from app.schemas.pagination import Page
 from app.services.customer_service import CustomerService
 from app.db.session import get_connection
-
+from app.dependencies.auth import get_current_user
+from app.models.user import User
 router = APIRouter()
 
 @router.post("/", response_model=CustomerRead)
-def create_customer(payload: CustomerCreate, db: Session = Depends(get_connection)):
-    return CustomerService.create_customer(db, payload)
+def create_customer(
+        payload: CustomerCreate,
+        db: Session = Depends(get_connection),
+        current_user: User =  Depends(get_current_user)
+):
+    return CustomerService.create_customer(db, payload, current_user)
 
 @router.get("/", response_model=Page[CustomerRead])
 def list_customers(
     filters: CustomerQuery = Depends(),
-    db: Session = Depends(get_connection)
+    db: Session = Depends(get_connection),
+    current_user: User = Depends(get_current_user)
 ):
-    return CustomerService.list_customers(db, filters)
+    return CustomerService.list_customers(db, filters, current_user)
 
 @router.get("/{customer_id}", response_model=CustomerRead)
-def get_customer(customer_id: int, db: Session = Depends(get_connection)):
-    customer = CustomerService.get_customer(db, customer_id)
+def get_customer(
+        customer_id: int,
+        db: Session = Depends(get_connection),
+        current_user: User = Depends(get_current_user)
+):
+    customer = CustomerService.get_customer(db, customer_id, current_user)
     if not customer:
         raise HTTPException(status_code=404, detail="Customer not found")
     return customer
 
 @router.put("/{customer_id}", response_model=CustomerRead)
-def update_customer(customer_id: int, payload: CustomerCreate, db: Session = Depends(get_connection)):
-    updated_customer = CustomerService.update_customer(db, customer_id, payload)
+def update_customer(
+        customer_id: int,
+        payload: CustomerCreate,
+        db: Session = Depends(get_connection),
+        current_user: User = Depends(get_current_user)
+):
+    updated_customer = CustomerService.update_customer(db, customer_id, payload, current_user)
     if not updated_customer:
         raise HTTPException(status_code=404, detail="Customer not found")
     return updated_customer
 
 @router.delete("/{customer_id}", response_model=bool)
-def delete_customer(customer_id: int, db: Session = Depends(get_connection)):
-    deleted = CustomerService.delete_customer(db, customer_id)
+def delete_customer(
+        customer_id: int,
+        db: Session = Depends(get_connection),
+        current_user: User = Depends(get_current_user)
+):
+    deleted = CustomerService.delete_customer(db, customer_id, current_user)
     if not deleted:
         raise HTTPException(status_code=404, detail="Customer not found")
     return deleted
