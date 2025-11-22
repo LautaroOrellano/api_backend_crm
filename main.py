6d808c1a1cde57756bd2aa2c98b713af464ef13c
Lautaro Orellano <Lautaa123@gmail.com>
2025-11-22 20:19:10 -0300

    refactor: new router user for endpoints

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.api.v1.customers import router as customer_router
+from app.api.v1.users import router as user_router
 from app.api.v1.auth import router as authorization_router
 app = FastAPI(title="Customer Manager API")
 
 # Registrar routers
 app.include_router(customer_router, prefix="/customers", tags=["Customers"])
-app.include_router(authorization_router)
+app.include_router(user_router, prefix="/users", tags=["Users"])
+app.include_router(authorization_router, prefix="/auth", tags=["auth"])
 
 
 @app.get("/")
