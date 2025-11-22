726d21dd3fc708d8d16e9e90895b1a329391885e
Lautaro Orellano <Lautaa123@gmail.com>
2025-11-21 22:23:48 -0300

    feat: new class for user in db

---
+++ app/models/user.py
@@ -0,0 +1,23 @@
+from app.db.base import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime, UTC
+from app.core.security import pwd_context
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    full_name = Column(String, nullable=True)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.now(UTC))
+    updated_at = Column(DateTime, default=datetime.now(UTC), onupdate=datetime.now(UTC))
+
+    # Métodos auxiliares
+    def verify_password(self, password: str) -> bool:
+        return pwd_context.verify(password, self.hashed_password)
+
+    def set_password(self, password: str):
+        self.hashed_password = pwd_context.hash(password)
