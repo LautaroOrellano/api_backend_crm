55f453c005254083439d32b01acb6ba70256280c
Lautaro Orellano <Lautaa123@gmail.com>
2025-11-22 20:46:29 -0300

    refactor: new endpoint logout

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,31 +1,40 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Form
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from datetime import timedelta
 from app.db.session import get_connection
 from app.models.user import User
+from app.core.jwt import get_jti_from_token
 from app.core.jwt import create_access_token
 from app.core.security import verify_password
 from app.config import settings
-from app.dependencies.auth import get_current_user
+from app.dependencies.auth import get_current_user, oauth2_scheme
 from app.schemas.user_schema import UserMe, Token
+from datetime import timedelta
+from app.services.revoked_token_service import RevokedTokenService
 
-router = APIRouter(prefix="/auth", tags=["auth"])
+router = APIRouter()
 
 
 @router.post("/login", response_model=Token)
 def login(username: str = Form(...), password: str = Form(...), db: Session = Depends(get_connection)):
+    # Buscar usuario
     user = db.query(User).filter(User.username == username).first()
     if not user or not verify_password(password, user.hashed_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Usuario o contraseña incorrectos")
 
+    # Crear token con jti y expiración
     access_token_expires = timedelta(minutes=int(settings.ACCESS_TOKEN_EXPIRE_MINUTES))
     access_token = create_access_token(
-        data={"sub": user.username, "role": getattr(user, "role", "user")},
-        expires_delta=access_token_expires
+        data={"sub": user.username, "role": getattr(user, "role", "user")}
     )
 
     return {"access_token": access_token, "token_type": "bearer"}
+
+@router.post("/logout")
+def logout(token: str = Depends(oauth2_scheme), db: Session = Depends(get_connection)):
+    jti = get_jti_from_token(token)
+    RevokedTokenService.revoke_token(db, jti)
+    return {"msg": "Logged out successfully"}
 
 @router.post("/token")
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_connection)):
