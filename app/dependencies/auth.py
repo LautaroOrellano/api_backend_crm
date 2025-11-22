3a9035ae887c4f4c028bc056c74795c7c71d0087
Lautaro Orellano <Lautaa123@gmail.com>
2025-11-22 20:41:51 -0300

    refactor: incorporation jti for captur the token logout

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -1,23 +1,29 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 from app.db.session import get_connection
 from app.models.user import User
-from app.config import settings
+from app.services.revoked_token_service import RevokedTokenService
+from app.core.jwt import decode_access_token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_connection)) -> User:
-    try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
-        user = db.query(User).filter(User.username == username).first()
-        if not user:
-            raise HTTPException(status_code=401, detail="User not found")
-        return user
-    except JWTError:
+    payload = decode_access_token(token)
+    if not payload:
         raise HTTPException(status_code=401, detail="Invalid token")
+
+    jti = payload.get("jti")
+    if RevokedTokenService.is_token_revoked(db, jti):
+        raise HTTPException(status_code=401, detail="Token revoked")
+
+    username: str = payload.get("sub")
+    if not username:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
 
