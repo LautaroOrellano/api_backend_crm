dc54b4d9f98fabbd45a57351ea18df9a565c6980
Lautaro Orellano <Lautaa123@gmail.com>
2025-11-21 23:31:36 -0300

    reafactor: new subdate for broken app

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,7 +5,11 @@ import os
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
 DB_NAME = os.getenv("DB_NAME", "imadwi_data_platform")
-DB_POOL_SIZE = int(os.getenv("DB_POOL_SIZE", "5"))
+DB_POOL_SIZE = int(os.getenv("DB_POOL_SIZE", 5))
+SECRET_KEY = os.getenv("SECRET_KEY", "@/MGBcbia$vAN2T")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
