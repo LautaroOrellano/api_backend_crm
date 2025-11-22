47ca3f4be6e9889f322f89d1ecc0ea62849072e2
Lautaro Orellano <Lautaa123@gmail.com>
2025-11-22 00:56:01 -0300

    refactor: add column role for table users

--- app/models/user.py
+++ app/models/user.py
@@ -14,6 +14,7 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.now(timezone.utc))
     updated_at = Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+    role = Column(String, default="admin")
 
     # Métodos auxiliares
     def verify_password(self, password: str) -> bool:
